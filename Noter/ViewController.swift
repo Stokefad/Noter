862c4c776a484c04d71273f8342583fdd5d2b402
Igor-Macbook Pro <igor.naum.2013@yandex.ru>
2018-11-15 23:34:10 +0300

    Initial Commit

---
+++ Noter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Noter
+//
+//  Created by Igor-Macbook Pro on 15/11/2018.
+//  Copyright © 2018 Igor-Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
